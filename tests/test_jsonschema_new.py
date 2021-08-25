34697c1a6773074c74e37cb1519047643fbe6dbf
Alzpetka <lili3.pokorna@seznam.cz>
2021-08-25 13:12:02 +0200

    jsonschema test

--- tests/test_jsonschema_new.py
+++ tests/test_jsonschema_new.py
@@ -23,13 +23,28 @@ def test_json(app):
     # data = json.loads('{"these": {"titles" : []}}')
     # validate(instance=data, schema=get_schema(), resolver=resolve)
     schema = app.extensions['invenio-records']
+    #nutne minimum
     data = json.loads('{"these": '
-                      '{"$schema":"https://narodni-repozitar.cz/schemas/nr_datasets_metadata/nr-datasets-metadata-v2.0.0.json",'
+                      '{'
+                      '"$schema":"https://narodni-repozitar.cz/schemas/nr_datasets_metadata/nr-datasets-metadata-v2.0.0.json",'
                       '"InvenioID": "1",'
-                      '"titles" : [{"title": {"cs" : "jeej"}, "titleType": "mainTitle"}]}'
+                      '"titles" : [{"title": {"cs" : "jeej"}, "titleType": "mainTitle"}], '
+                      '"creator" : ['
+                            '{"nameType": "Personal", '
+                            '"affiliaton": [{"termin": "termin"}], '
+                            '"fullName": "Alzbeta Pokorna", '
+                            '"authorityIdentifiers": [{"identifier": "jej", "scheme": "ORCID"}]'
+                            '}'
+                        '],'
+                      '"dateAvailable": "1970",'
+                      '"resourceType": [{"termin": "termin"}],'
+                      '"accessRights" : [{"termin": "termin"}],'
+                      '"abstract" : {"cs": "kchc"},'
+                      '"subjectCategory": [{"termin": "termin"}] '
+                      '}'
                       '}')
     #data = json.loads('{"these": {"titles" : [{"title" :{"cs": "jej", "en": "yay"}, "titleType": "mainTitle"}]}}')
-    #schema.validate(data, get_schema(), cls=AllOfDraft7Validator)
+    schema.validate(data, get_schema(), cls=AllOfDraft7Validator)
 
     # data = json.loads('{"these": {"abstract" : {"cs": "jej", "en": "yay"}, "contributor" : "Alzbeta Pokorna", "modified":"2012-04-23T18:25:43.511Z"}}')
     # schema.validate(data, get_schema())
